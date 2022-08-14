8d14748a9f534e1eaab79fe57b2013edbf08365e
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-08-14 21:59:46 +0300

    one to many basic example

--- project/app_one_onetomany/models.py
+++ project/app_one_onetomany/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Lesson(models.Model):
+    lesson_name = models.CharField(max_length=50)
+    lesson_teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.lesson_name}-Teacher({self.lesson_teacher})"
--- project/app_one_onetomany/views.py
+++ project/app_one_onetomany/views.py
@@ -1,3 +1,15 @@
+from multiprocessing import context
+import re
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def index(request):
+
+    lesson = Lesson.objects.all()
+
+    context = {
+        "lesson" : lesson
+    }
+
+    return render(request, "index.html", context)
