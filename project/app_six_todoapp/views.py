8bfd203d09e364b92914812f61e0be1313240ffd
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-10-27 12:38:39 +0300

    basic tasks creation process done

--- project/app_six_todoapp/views.py
+++ project/app_six_todoapp/views.py
@@ -1,6 +1,15 @@
+from asyncio import tasks
+from multiprocessing import context
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def todo(request):
-    return render(request, 'todo.html')
+
+    tasks = Task.objects.all()
+
+    context = {
+        "tasks" : tasks
+    }
+
+    return render(request, 'todo.html', context)
