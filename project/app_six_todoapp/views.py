1f4b2e896698124c5bece5894b25ecbc1508345e
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-10-27 12:58:56 +0300

    Crud C(Create) completed for TODO

--- project/app_six_todoapp/views.py
+++ project/app_six_todoapp/views.py
@@ -1,15 +1,25 @@
 from asyncio import tasks
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 # Create your views here.
 
 def todo(request):
 
     tasks = Task.objects.all()
 
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/todoapp")
+
     context = {
-        "tasks" : tasks
+        "tasks" : tasks,
+        'form' : form,
     }
 
     return render(request, 'todo.html', context)
